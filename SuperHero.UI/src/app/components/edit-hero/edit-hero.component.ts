fe751b8561a91f6ffb5f82495c75f713dc5cf388
albixhuveli <albi.xhuveli.601@gmail.com>
2023-08-30 01:56:05 -0400

    added button interface

--- SuperHero.UI/src/app/components/edit-hero/edit-hero.component.ts
+++ SuperHero.UI/src/app/components/edit-hero/edit-hero.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SuperHero } from 'src/app/models/super-hero';
+import { SuperHeroService } from 'src/app/services/super-hero.service';
 
 @Component({
   selector: 'app-edit-hero',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-hero.component.css']
 })
 export class EditHeroComponent {
+
+  @Input() hero?: SuperHero;
+  @Output() heroesUpdated = new EventEmitter<SuperHero[]>();
+
+  constructor(private superHeroService: SuperHeroService) {}
+
+  ngOnItit(): void {}
+
+  updateHero(hero: SuperHero) {
+    this.superHeroService
+      .updateHero(hero)
+      .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes));
+  }
+
+  deleteHero(hero: SuperHero) {
+    this.superHeroService
+      .deleteHero(hero)
+      .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes));
+  }
+
+  createHero(hero: SuperHero) {
+    this.superHeroService
+      .createHero(hero)
+      .subscribe((heroes: SuperHero[]) => this.heroesUpdated.emit(heroes));
+  }
 
 }
--- SuperHero.UI/src/app/services/super-hero.service.ts
+++ SuperHero.UI/src/app/services/super-hero.service.ts
@@ -9,13 +9,37 @@ import { environment } from 'src/environments/environment.development';
   providedIn: 'root'
 })
 export class SuperHeroService {
+  deleteHero(hero: SuperHero) {
+    throw new Error('Method not implemented.');
+  }
   private url = "SuperHero"; 
 
   constructor(private http: HttpClient) { }
 
   public getSuperHeroes() : Observable<SuperHero[]> {
-    
-    return this.http.get<SuperHero[]>(`${environment.apiUrl}/${this.url}`);
+    return this.http.get<SuperHero[]>(
+      `${environment.apiUrl}/${this.url}`);
+  }
+
+  public updateHero(hero: SuperHero): Observable<SuperHero[]> {
+    return this.http.put<SuperHero[]>(
+      `${environment.apiUrl}/${this.url}`,
+      hero
+      );
 
   }
+  public createHero(hero: SuperHero): Observable<SuperHero[]> {
+    return this.http.post<SuperHero[]>(
+      `${environment.apiUrl}/${this.url}`,
+      hero
+      );
+
+  }
+  public deleteSuperHeroes(hero: SuperHero) : Observable<SuperHero[]> {
+    return this.http.delete<SuperHero[]>(
+      `${environment.apiUrl}/${this.url}/${hero.id}`
+      );
+
+  }
+
 }
